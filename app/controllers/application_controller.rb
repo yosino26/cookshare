a8f99a9be531be5cd2bbd434bf7f611dfc74ff55
yosino26 <fn.yoshino57@gmail.com>
2025-08-14 06:21:26 +0900

    ストロングパラメータの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+    # CSRF保護
+    protect_from_forgery with: :exception
+  
+    # Devise用の設定
+    before_action :authenticate_user!, except: [:index, :show]
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  
+    private
+  
+    def configure_permitted_parameters
+      # 新規登録時にnameパラメータを許可
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      # アカウント更新時にnameパラメータを許可
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
 end
