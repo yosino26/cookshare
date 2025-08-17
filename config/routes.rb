2d86488eded2f20b914c302fac51b7781682e2ae
yosino26 <fn.yoshino57@gmail.com>
2025-08-18 07:00:22 +0900

    ルーティング更新

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root 'recipes#index'  # トップページをレシピ一覧に
   
-  # 後でresourcesに変更予定
-  resources :recipes, only: [:index, :show]
+  # RESTfulなレシピルーティング
+  resources :recipes do
+    # 検索機能（/recipes/search）
+    collection do
+      get :search
+    end
+  end
+  
+  # ユーザーのプロフィールページ
+  resources :users, only: [:show]
 end
