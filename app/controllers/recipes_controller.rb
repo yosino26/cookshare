db92fcec90163a45abdb85f4a7c6e56cb5fe680a
yosino26 <fn.yoshino57@gmail.com>
2025-08-18 18:43:41 +0900

    RecipesControllerの更新

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,12 +1,59 @@
 class RecipesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   def index
-    # 後でレシピデータを取得する処理を追加
-    @recipes = []  # 現在は空配列
+    @recipes = Recipe.includes(:user).recent.limit(12)
+    # includes(:user) でN+1問題を防ぐ
   end
 
   def show
+    # @recipeは before_action で設定済み
+  end
+
+  def new
+    @recipe = current_user.recipes.build
+  end
+
+  def create
+    @recipe = current_user.recipes.build(recipe_params)
+    
+    if @recipe.save
+      redirect_to @recipe, notice: 'レシピが投稿されました！'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    # @recipeは before_action で設定済み
+  end
+
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to @recipe, notice: 'レシピが更新されました！'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @recipe.destroy
+    redirect_to recipes_path, notice: 'レシピが削除されました。'
+  end
+  
+
+  private
+  def set_recipe
     @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :description, :cooking_time, :servings, :image)
+  end
+
+  def correct_user
+    redirect_to recipes_path unless @recipe.user == current_user
   end
 end
