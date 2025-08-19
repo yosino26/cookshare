74c999a2f518e411860788edd39e63f3eb00b813
yosino26 <fn.yoshino57@gmail.com>
2025-08-20 05:09:24 +0900

    JavaScript設定

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,20 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// Railsの基本機能を有効化
+import Rails from "@rails/ujs"
+import Turbo from "@hotwired/turbo-rails"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+// Bootstrap
+import "bootstrap"
+import "../stylesheets/application"
+
+// カスタムJavaScript
+import "./recipe_form"
+
+Rails.start()
+Turbo.start()
+ActiveStorage.start()
