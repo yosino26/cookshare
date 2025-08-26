b56c866537eba269c442dd2bac3a13f9946abcb5
yosino26 <fn.yoshino57@gmail.com>
2025-08-26 12:41:32 +0900

    CSSを更新

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,20 +1,17 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// Turbo & Stimulus
 import "@hotwired/turbo-rails"
 import "controllers"
 
-// Railsの基本機能を有効化
-import Rails from "@rails/ujs"
-import Turbo from "@hotwired/turbo-rails"
+// Active Storage
 import * as ActiveStorage from "@rails/activestorage"
-import "channels"
 
-// Bootstrap
-import "bootstrap"
-import "../stylesheets/application"
+// Bootstrap（Popper → Bootstrap の順で）
 
-// カスタムJavaScript
+
+// （使っていれば）ActionCable
+// import "channels"
+
+// Custom JS
 import "./recipe_form"
 
-Rails.start()
-Turbo.start()
 ActiveStorage.start()
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+pin "bootstrap" # @5.3.7
+pin "@rails/activestorage", to: "@rails--activestorage.js" # @8.0.201
