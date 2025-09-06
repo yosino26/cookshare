c5d46672a11f7f57af23e7705b76c2c49a2f1f54
yosino26 <fn.yoshino57@gmail.com>
2025-09-07 06:17:11 +0900

    ルーティングを定義とレシピ詳細ページに評価機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
     end
     resource :favorite, only: [:create, :destroy]  # /recipes/:recipe_id/favorite
     resources :comments, only: [:create, :destroy]  # 追加
+    resources :ratings, only: [:create]  # 追加
   end
 
   # ユーザー（プロフィール表示・編集・更新・お気に入り一覧）
