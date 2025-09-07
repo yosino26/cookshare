031cf681f9ff7074b0f7915c700b76eb0ee934be
yosino26 <fn.yoshino57@gmail.com>
2025-09-08 05:47:25 +0900

    Followモデル実装と Userモデルにフォロー機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -50,4 +50,35 @@ class User < ApplicationRecord
   has_many :ratings, dependent: :destroy
   has_many :rated_recipes, through: :ratings, source: :recipe
 
+  # フォロー機能
+  has_many :active_follows, class_name: 'Follow', foreign_key: 'follower_id', dependent: :destroy
+  has_many :passive_follows, class_name: 'Follow', foreign_key: 'following_id', dependent: :destroy
+
+  # フォローしているユーザー
+  has_many :followings, through: :active_follows, source: :following
+  # フォロワー（自分をフォローしているユーザー）
+  has_many :followers, through: :passive_follows, source: :follower
+
+  # フォロー関連のメソッド
+  def follow(user)
+    return false if user == self
+    active_follows.find_or_create_by(following: user)
+  end
+
+  def unfollow(user)
+    active_follows.find_by(following: user)&.destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
+
+  def follower_count
+    followers.count
+  end
+
+  def following_count
+     followings.count
+  end
+
 end
