858ac4b7956fd243b2a831985f44840c835f82a4
yosino26 <fn.yoshino57@gmail.com>
2025-09-11 16:30:56 +0900

    各モデル設定
    recipeとrecipe_categoryの関連付け

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,12 @@
 class Category < ApplicationRecord
+  has_many :recipe_categories, dependent: :destroy
+  has_many :recipes, through: :recipe_categories
+  
+  validates :name, presence: true, uniqueness: true
+  
+  scope :with_recipes, -> { joins(:recipes).distinct }
+  
+  def recipe_count
+    recipes.count
+  end
 end
