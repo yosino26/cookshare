7cae8a86d90e582a8a3a0cfa1b8be5f1783e6650
yosino26 <fn.yoshino57@gmail.com>
2025-10-18 09:08:58 +0900

    S#連携のための環境ファイル設定

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,6 +3,7 @@ import "@hotwired/turbo-rails"
 
 // Active Storage
 import * as ActiveStorage from "@rails/activestorage"
+ActiveStorage.start()
 
 // Popper → Bootstrap（順番厳守）
 import "@popperjs/core"
@@ -18,4 +19,3 @@ import "bootstrap"
 import "./nested_fields"
 
 // ActiveStorageは最後に1回だけ起動
-ActiveStorage.start()
