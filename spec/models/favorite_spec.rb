4c94f007de355be98e32207ab2b59e1cb5395b2e
yosino26 <fn.yoshino57@gmail.com>
2025-10-31 02:34:38 +0900

    Favoriteモデル同じユーザーが同じレシピを二重にお気に入りできない事をテスト

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Favorite, type: :model do
-  it { should belong_to(:user) }
-  it { should belong_to(:recipe) }
+  it { should belong_to(:user).required }
+  it { should belong_to(:recipe).required }
 
   before { create(:favorite) }  # 既存1件
 
