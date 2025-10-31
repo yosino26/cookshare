ed3e11112fe6bc356feeb335f49c81a4f30b79c3
yosino26 <fn.yoshino57@gmail.com>
2025-11-01 01:10:01 +0900

    Commentモデルに、本文が空はNGであることをテスト

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe Comment, type: :model do
   it { should belong_to(:recipe) }
   it { should validate_presence_of(:content) }  # ← body → content に変更
 
+  it "空白のみはNG" do
+    expect(build(:comment, content: " \n\t ")).to be_invalid
+  end
+
   it "レシピ削除で巻き添え削除" do
     recipe = create(:recipe)
     create(:comment, recipe: recipe)
