7b108d3e06623e5226d995bc743cb85734b2d9f4
yosino26 <fn.yoshino57@gmail.com>
2025-11-19 01:50:44 +0900

    Usersのリクエストテストを実装に合わせて修正

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,25 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /show" do
+  let(:user) { create(:user) }
+
+  describe "GET /users/:id (show)" do
     it "returns http success" do
-      get "/users/show"
-      expect(response).to have_http_status(:success)
+      get user_path(user)
+      expect(response).to have_http_status(:success) # 200
     end
   end
 
-  describe "GET /edit" do
-    it "returns http success" do
-      get "/users/edit"
-      expect(response).to have_http_status(:success)
+  describe "GET /users/:id/edit (edit)" do
+    context "when signed in" do
+      it "returns http success" do
+        sign_in user
+        get edit_user_path(user)
+        expect(response).to have_http_status(:success) # 200
+      end
+    end
+
+    context "when not signed in" do
+      it "redirects to sign in" do
+        get edit_user_path(user)
+        expect(response).to have_http_status(:found)   # 302
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
   end
 
-  describe "GET /update" do
-    it "returns http success" do
-      get "/users/update"
-      expect(response).to have_http_status(:success)
+  describe "PATCH /users/:id (update)" do
+    context "when signed in" do
+      it "updates and redirects to show" do
+        sign_in user
+        patch user_path(user), params: { user: { name: "NewName" } }
+        expect(response).to have_http_status(:found)   # 302
+        expect(response).to redirect_to(user_path(user))
+
+        follow_redirect!
+        expect(response).to have_http_status(:success) # 200
+        expect(response.body).to include("NewName")
+      end
+    end
+
+    context "when not signed in" do
+      it "redirects to sign in" do
+        patch user_path(user), params: { user: { name: "NewName" } }
+        expect(response).to have_http_status(:found)   # 302
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
   end
-
 end
