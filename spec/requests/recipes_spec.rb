949b8f501cfd0b766697194001ed9e67354cd007
yosino26 <fn.yoshino57@gmail.com>
2025-12-04 00:18:43 +0900

    requests/recipes_spec.rbの一覧ページが表示されることが確認

--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Recipes", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/recipes/index"
-      expect(response).to have_http_status(:success)
+  describe "GET /recipes" do
+    it "一覧ページが表示されること" do
+      get recipes_path
+      expect(response).to have_http_status(:ok) # 200
     end
   end
-
 end
