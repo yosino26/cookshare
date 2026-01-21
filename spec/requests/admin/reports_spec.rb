4b9d858e085adfd5bea5c9d6d987e6534519096e
yosino26 <fn.yoshino57@gmail.com>
2026-01-21 13:22:57 +0900

    spec/requests/admin/reports_spec.rbのテスト準備

---
+++ spec/requests/admin/reports_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.describe "Admin::Reports", type: :request do
+  describe "GET /admin/reports" do
+    let(:admin) { create(:user, :admin) }
+    let(:user)  { create(:user) }
+
+    context "認可" do
+      it "管理者は 200 OK" do
+        sign_in admin
+        get admin_reports_path
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "一般ユーザーは root にリダイレクトされる" do
+        sign_in user
+        get admin_reports_path
+
+        expect(response).to have_http_status(:found)
+        expect(response).to redirect_to(root_path)
+        expect(flash[:alert]).to eq "管理者権限が必要です"
+      end
+
+
+ 
+    end
+  end
+end
