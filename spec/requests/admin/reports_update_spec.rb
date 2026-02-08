675fb0e00d6e1fc349a08c2f44960ba2dab98389
yosino26 <fn.yoshino57@gmail.com>
2026-02-09 02:56:38 +0900

    管理者の通報更新処理のリクエストテストを追加の準備２
    管理者用の通報更新アクションについて、
    未ログイン・一般ユーザー時のリダイレクト挙動を
    request specで確認できるようにした

--- spec/requests/admin/reports_update_spec.rb
+++ spec/requests/admin/reports_update_spec.rb
@@ -4,5 +4,24 @@ RSpec.describe "Admin::Reports 更新系", type: :request do
   let(:admin) { create(:user, :admin) }
   let(:user)  { create(:user) }
 
+  # factories/reports.rb が reportable: recipe を作るのでそのまま使う
+  let!(:report) { create(:report, status: :pending) }
+
+  let(:update_path)      { admin_report_path(report) }
+  let(:investigate_path) { investigate_admin_report_path(report) }
+  let(:resolve_path)     { resolve_admin_report_path(report) }
+  let(:dismiss_path)     { dismiss_admin_report_path(report) }
+
+  shared_examples "未ログインはログイン画面へ" do |verb, path_proc, params = {}|
+    it "302でログイン画面へリダイレクトされる" do
+      public_send(verb, instance_exec(&path_proc), params: params)
+      expect(response).to have_http_status(:found)
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
+
+  shared_examples "一般ユーザーはrootへ" do |verb, path_proc, params = {}|
+    before { sign_in user }
+
 
 end
