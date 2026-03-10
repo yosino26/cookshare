9f70de3e9da3ce21e38c56dac681c71b1762419a
yosino26 <fn.yoshino57@gmail.com>
2026-03-11 01:28:13 +0900

    admin/users 更新系の未ログインテスト追加1
    未ログイン時はログイン画面へリダイレクトされ、DBが更新されないことを共通テストで確認するようにした

--- spec/requests/admin/users_update_spec.rb
+++ spec/requests/admin/users_update_spec.rb
@@ -10,4 +10,23 @@ RSpec.describe "Admin::Users 更新系", type: :request do
   let(:unsuspend_path)    { unsuspend_admin_user_path(user) }
   let(:promote_path)      { promote_admin_user_path(user) }
 
+  shared_examples "未ログインはログイン画面へ" do |verb, path_proc, params = nil|
+    it "302でログイン画面へリダイレクトされ、DBは更新されない" do
+      original_attrs = user.attributes.slice("name", "email", "admin", "suspended", "suspended_until", "suspend_reason")
+
+      if params
+        public_send(verb, instance_exec(&path_proc), params: params)
+      else
+        public_send(verb, instance_exec(&path_proc))
+      end
+
+      expect(response).to have_http_status(:found)
+      expect(response).to redirect_to(new_user_session_path)
+
+      user.reload
+      expect(user.attributes.slice("name", "email", "admin", "suspended", "suspended_until", "suspend_reason"))
+        .to eq(original_attrs)
+    end
+  end
+
 end
