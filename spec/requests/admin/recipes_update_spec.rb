9f1bbe1fcb584d1b6fb0ae9f18a5efc3ddf577d8
yosino26 <fn.yoshino57@gmail.com>
2026-04-07 04:28:15 +0900

    管理画面のレシピ更新処理のテストを追加２
    管理画面のレシピ更新系テストを追加
    一般ユーザーはトップページへ戻されるテストを追加
    一般ユーザーはレシピを変更できないことを確認
    削除しても件数が変わらないことを確認
    更新系処理でDBが変わらないことを確認

--- spec/requests/admin/recipes_update_spec.rb
+++ spec/requests/admin/recipes_update_spec.rb
@@ -33,6 +33,32 @@ RSpec.describe "Admin::Recipes 更新系", type: :request do
     end
   end
 
+  shared_examples "一般ユーザーはrootへ" do |verb, path_proc, params = nil|
+    it "302でrootへリダイレクトされ、DBは変更されない" do
+      sign_in create(:user)
+
+      original_attrs = recipe.attributes.slice("title", "description", "cooking_time", "servings", "hidden")
+      original_count = Recipe.count
+
+      if params
+        public_send(verb, instance_exec(&path_proc), params: params)
+      else
+        public_send(verb, instance_exec(&path_proc))
+      end
+
+      expect(response).to have_http_status(:found)
+      expect(response).to redirect_to(root_path)
+
+      if verb == :delete
+        expect(Recipe.count).to eq(original_count)
+      else
+        recipe.reload
+        expect(recipe.attributes.slice("title", "description", "cooking_time", "servings", "hidden"))
+          .to eq(original_attrs)
+      end
+    end
+  end
+
 
 
   
