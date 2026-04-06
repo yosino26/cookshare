76ab8b8284d375d2de61d2c0c7a469fdb1d4c988
yosino26 <fn.yoshino57@gmail.com>
2026-04-06 11:18:31 +0900

    管理画面のレシピ更新処理のテストを追加１
    管理画面のレシピ更新系リクエストスペックを追加
    未ログイン時にログイン画面へリダイレクトされるテストを追加
    未ログイン時にレシピ情報が変更されないことを確認
    削除処理で件数が変わらないことを確認
    更新・非表示・再表示でDBが変わらない共通テストを追加

--- spec/requests/admin/recipes_update_spec.rb
+++ spec/requests/admin/recipes_update_spec.rb
@@ -10,7 +10,30 @@ RSpec.describe "Admin::Recipes 更新系", type: :request do
   let(:unhide_path)  { unhide_admin_recipe_path(recipe) }
 
   shared_examples "未ログインはログイン画面へ" do |verb, path_proc, params = nil|
+    it "302でログイン画面へリダイレクトされ、DBは変更されない" do
+      original_attrs = recipe.attributes.slice("title", "description", "cooking_time", "servings", "hidden")
+      original_count = Recipe.count
 
+      if params
+        public_send(verb, instance_exec(&path_proc), params: params)
+      else
+        public_send(verb, instance_exec(&path_proc))
+      end
 
+      expect(response).to have_http_status(:found)
+      expect(response).to redirect_to(new_user_session_path)
 
+      if verb == :delete
+        expect(Recipe.count).to eq(original_count)
+      else
+        recipe.reload
+        expect(recipe.attributes.slice("title", "description", "cooking_time", "servings", "hidden"))
+          .to eq(original_attrs)
+      end
+    end
   end
+
+
+
+  
+end
