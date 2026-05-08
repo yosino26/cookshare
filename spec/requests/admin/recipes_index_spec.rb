0c4bc13892c42be80723a4f8f9f48b1bbbbade2f
yosino26 <fn.yoshino57@gmail.com>
2026-05-09 01:37:01 +0900

    管理者レシピ一覧のリクエストテスト１
    - 未ログインはログイン画面へリダイレクトされる
    - 一般ユーザーはrootへリダイレクトされる

--- spec/requests/admin/recipes_index_spec.rb
+++ spec/requests/admin/recipes_index_spec.rb
@@ -4,4 +4,26 @@ RSpec.describe "Admin::Recipes 一覧系", type: :request do
   let(:admin) { create(:user, :admin) }
   let(:user)  { create(:user) }
 
+  describe "GET /admin/recipes" do
+    context "未ログイン" do
+      it "ログイン画面へリダイレクトされる" do
+        get admin_recipes_path
+
+        expect(response).to have_http_status(:found)
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+    context "一般ユーザー" do
+      before do
+        sign_in user
+      end
+
+      it "rootへリダイレクトされる" do
+        get admin_recipes_path
+
+        expect(response).to have_http_status(:found)
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
 end
