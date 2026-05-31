c26d81016b752375ffc63d02fa7348f4955ec283
yosino26 <fn.yoshino57@gmail.com>
2026-05-31 11:56:29 +0900

    管理者用レシピCSV出力のテスト１
    ログイン画面へリダイレクトされることを確認

--- spec/requests/admin/recipes_export_spec.rb
+++ spec/requests/admin/recipes_export_spec.rb
@@ -4,6 +4,14 @@ require "csv"
 RSpec.describe "Admin::Recipes CSVエクスポート", type: :request do
   let(:admin) { create(:user, :admin) }
   let(:user)  { create(:user, email: "user@example.com") }
+  describe "GET /admin/recipes/export" do
+    context "未ログイン" do
+      it "ログイン画面へリダイレクトされる" do
+        get admin_recipes_export_path(format: :csv)
 
-
+        expect(response).to have_http_status(:found)
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
+
+end
