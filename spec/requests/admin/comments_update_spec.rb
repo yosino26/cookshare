167076b9d46736d361883be72f9215d13aa25d49
yosino26 <fn.yoshino57@gmail.com>
2026-06-14 22:24:36 +0900

    管理者コメント更新処理のリクエストテスト1
    ログイン画面へリダイレクトされる事を確認

--- spec/requests/admin/comments_update_spec.rb
+++ spec/requests/admin/comments_update_spec.rb
@@ -7,4 +7,12 @@ RSpec.describe "Admin::Comments 更新系", type: :request do
     let!(:comment) { create(:comment, recipe: recipe, user: user, hidden: false) }
 
       end
+      context "未ログイン" do
+        it "ログイン画面へリダイレクトされる" do
+          patch hide_admin_comment_path(comment)
+  
+          expect(response).to have_http_status(:found)
+          expect(response).to redirect_to(new_user_session_path)
+        end
+      end
     end
